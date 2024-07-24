2cb54598af0b72d5e4c7aa3cbcf7a69afc3a5faa
step-to-the-future <165415257+step-to-the-future@users.noreply.github.com>
2024-07-24 12:35:23 +0300

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,37 @@
+function toggleanswer() {
+  document.querySelector('.navbar').classList.toggle('active');
+    document.querySelector('.menu-icon').classList.toggle('active');
+  };
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+
+    if(top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+      links.classList.remove('active');
+      document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });   
+    };
+  });
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+
+/*     Typing script     */
+const typed = new Typed('.multiple-text', {
+  strings: ['Front-tend разработчик', 'Гений', 'Филантроп', 'Вообщем Джони Синс'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
